2515734eb5cbe5cbaf8dc0c484932c120b1ac1d5
Enigma1123 <91005881+Enigma1123@users.noreply.github.com>
2021-09-26 22:37:29 +0300

    Update Task6.cpp

--- 27.09.2021-Homework/Task6/Task6.cpp
+++ 27.09.2021-Homework/Task6/Task6.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
-	int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	int x1 = 0;
+	int y1 = 0;
+	int x2 = 0;
+	int y2 = 0;
 	cin >> x1 >> y1 >> x2 >> y2;
 	if ((x1 == x2) or (y1 == y2) or (abs(x1 - x2) == abs(y1 - y2)))
 	{
