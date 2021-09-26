b90e057e26c66befa56a5625d74b54d431d2aff2
Enigma1123 <91005881+Enigma1123@users.noreply.github.com>
2021-09-26 19:12:52 +0300

    Add files via upload

---
+++ 27.09.2021-Homework/Task7/Task7.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0, m = 0, k = 0;
+	cin >> n >> m >> k;
+	if (((k % n == 0) or (k % m == 0)) and (k <= (n * m)))
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
---
+++ 27.09.2021-Homework/Task8/Task8.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0, b = 0, c = 0;
+	cin >> a >> b >> c;
+	if ((a >= b + c) or (b >= a + c) or (c >= a + b) or (a <= 0) or (b <= 0) or (c <= 0))
+	{
+	cout << "impossible";
+	}
+	else if ((a * a + b * b == c * c) or (b * b + c * c == a * a) or (a * a + c * c == b * b))
+	{
+		cout << "right";
+	}
+	else if ((a * a + b * b > c * c) or (b * b + c * c > a * a) or (a * a + c * c > b * b))
+	{
+		cout << "acute";
+	}
+	else if ((a * a + b * b < c * c) or (b * b + c * c < a * a) or (a * a + c * c < b * b))
+	{
+		cout << "obtuse";
+	}
+}
