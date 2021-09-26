0c8fdd5fcf2c274e18858c67379c0ba6e2dd6edc
Enigma1123 <91005881+Enigma1123@users.noreply.github.com>
2021-09-26 23:08:47 +0300

    Add files via upload

---
+++ 22.09.2021-Homework/Task1/Task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	cin >> a;
+	b = a + 1;
+	c = a - 1;
+	cout << "The next number for the number " << a << " is " << b << "." << endl;
+	cout << "The previous number for the number " << a << " is " << c << ".";
+}
