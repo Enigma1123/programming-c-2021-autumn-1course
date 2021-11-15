d2de9dab4b423346b32c86f456ab77088b3d9d06
Enigma1123 <91005881+Enigma1123@users.noreply.github.com>
2021-11-15 13:42:17 +0300

    Upload

---
+++ Examination/Task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int min = 0;
+	int max = 0;
+	int sum = 0;
+	int i = 2;
+	cin >> min >> max;
+	while // Не знаю как сделать так чтобы останавливать прием чисел
+	{
+		i++;
+		if (i % 2 == 0)
+		{
+			if (a < min) min = a;
+		}
+		else if (a > max) max = a;
+	}
+	sum = min + max;
+	cout << sum; 
+}
---
+++ Examination/Task3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int a1 = 0;
+	int b1 = 0;
+	int nok = 0;
+	int nod = 0;
+	cin >> a >> b;
+	a1 = a;
+	b1 = b;
+    while ((a1 != 0) and (b1 != 0))
+    {
+        if (a1 > b1) a1 = a1 % b1;
+        else  b1 = b1 % a1;
+    }
+	nod = a1 + b1;
+	nok = (a * b) / nod;
+	cout << nok;
+}
